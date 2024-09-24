2f672d073029352593562eebcf0100f107b9e71d
Neetu Singh <neetuisngh99590@gmail.com>
2024-09-24 08:48:41 +0530

    fixed

--- script.js
+++ script.js
@@ -1,161 +1,116 @@
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// Toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-// scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// Scroll sections
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            // active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll
-            sec.classList.add('show-animate');
-        }
-        // if want to animation that repeats on scroll use this
-        else {
-            sec.classList.remove('show-animate');
-        }
-    });
+    if (top >= offset && top < offset + height) {
+      // Active navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+      // Active sections for animation on scroll
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
 
-    // sticky navbar
-    let header = document.querySelector('header');
+  // Sticky navbar
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+  // Remove toggle icon and navbar when clicking navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 
-    // remove toggle icon and navbar when click navbar links (scroll)
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
-    // animation footer on scrollc
-    let footer = document.querySelector('footer');
-
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
-}
-function openLive2() {
-    let livelink2 = "https://incomparable-cupcake-0fd9e6.netlify.app/";
-    window.open(livelink2, '_blank');
-}
-function openCode2() {
-    let code2 = "https://github.com/Neetsin/Javascript";
-    window.open(code2, '_blank');
-}
-
-function openLive1() {
-
-    let liveLink = "https://prismatic-monstera-e18d13.netlify.app/";
-
-    window.open(liveLink, '_blank');
-}
-
-function openCode1() {
-    let codeLink = "https://github.com/Neetsin/Movie-app";
-
-    window.open(codeLink, '_blank');
-}
-
-function openLive() {
-    window.open("https://library-management-system-mauve.vercel.app/", "_blank");
-}
-
-function openCode() {
-    window.open("https://github.com/Neetsin/Library-Management-System", "_blank");
-}
-function openCode3() {
-    let code3 = "https://github.com/Neetsin/BestbuyClone";
-    window.open(code3, "_blank");
-}
-
-function openLive3() {
-    let Live3 = "https://dainty-tiramisu-f10af7.netlify.app/";
-    window.open(Live3, "_blank");
-}
-
+  // Animation footer on scroll
+  let footer = document.querySelector("footer");
+  footer.classList.toggle(
+    "show-animate",
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
+};
 
 document.addEventListener("DOMContentLoaded", function () {
-    var aboutImg = document.getElementById("abut-img");
+  var aboutImg = document.getElementById("abut-img");
 
-    if (document.body.id === "home-page" && aboutImg) {
-        aboutImg.style.display = "block";
-    } else if (aboutImg) {
-        aboutImg.style.display = "none";
-    }
+  if (document.body.id === "home-page" && aboutImg) {
+    aboutImg.style.display = "block";
+  } else if (aboutImg) {
+    aboutImg.style.display = "none";
+  }
 });
+
 function downloadResume() {
+  var resumePath =
+    "https://drive.google.com/file/d/1tUeirJmUH43RA_6xg6uKx2ZaTWPnfSBS/view?usp=sharing";
 
-    var resumePath = 'https://drive.google.com/file/d/1tUeirJmUH43RA_6xg6uKx2ZaTWPnfSBS/view?usp=sharing';
+  var link = document.createElement("a");
+  link.href = resumePath;
+  link.download = "YourResumeFileName.pdf";
+  document.body.appendChild(link);
 
-
-    var link = document.createElement('a');
-    link.href = resumePath;
-    link.download = 'YourResumeFileName.pdf';
-    document.body.appendChild(link);
-
-
-    link.click();
-
-
-    document.body.removeChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const scrollLinks = document.querySelectorAll('a[href^="#"]');
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollLinks = document.querySelectorAll('a[href^="#"]');
 
-    scrollLinks.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
+  scrollLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
 
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
 
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth",
         });
+      }
     });
+  });
 });
+
 // Arrow button element
-let arrowButton = document.querySelector('.footer-iconTop a');
+let arrowButton = document.querySelector(".footer-iconTop a");
 
+arrowButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-arrowButton.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    // Scroll smoothly to the top of the page
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  // Scroll smoothly to the top of the page
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
 
-const techItems = document.querySelectorAll('.tech-item');
-const techNameDisplay = document.getElementById('tech-name-display');
+const techItems = document.querySelectorAll(".tech-item");
+const techNameDisplay = document.getElementById("tech-name-display");
 
-techItems.forEach(item => {
-    item.addEventListener('click', () => {
-        const techName = item.getAttribute('data-tech');
-        techNameDisplay.textContent = techName;
-    });
+techItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const techName = item.getAttribute("data-tech");
+    techNameDisplay.textContent = techName;
+  });
 });
-
-
-
-
